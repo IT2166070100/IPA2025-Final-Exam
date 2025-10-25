89ef29d82672442c997a5294f00d9f6101cc2471
IT2166070100 <66070100@kmitl.ac.th>
2025-10-25 10:55:31 +0700

    refactor: remove unused code in netmiko

--- netmiko_final.py
+++ netmiko_final.py
@@ -12,31 +12,6 @@ device_params = {
     "password": password,
 }
 
-
-def gigabit_status():
-    ans = ""
-    with ConnectHandler(**device_params) as ssh:
-        up = 0
-        down = 0
-        admin_down = 0
-        result = ssh.send_command("sh int", use_textfsm=True)
-        for status in result:
-            if status["interface"].startswith("GigabitEthernet"):
-                ans += f"{status['interface']} {status['link_status']},"
-                if status['link_status'] == "up":
-                    up += 1
-                elif status['link_staus'] == "down":
-                    down += 1
-                elif status['link_status'] == "administratively down":
-                    admin_down += 1
-        ans = f"{ans[:-1]} -> {up} up, {down} down, {admin_down} administratively down"
-        pprint(ans)
-        return ans
-
-
-from netmiko import ConnectHandler
-
-
 def gigabit_status(router_ip):
     device_params = {
         "device_type": "cisco_ios",
